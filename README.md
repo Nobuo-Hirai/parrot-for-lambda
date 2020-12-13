e01f8c3d28a69b76f6a2621ee249ed86b931f65b
Nobuo-Hirai <nobuo.hirai@icare.jpn.com>
2020-12-13 21:53:46 +0900

    parrot for lambda作成

---
+++ README.md
@@ -0,0 +1,42 @@
+# parrot-for-Lambda
+
+## Description
+AWS Lambdaで動作するSlack連携アプリ  
+SlackでAppにメンションをつけてメッセージを送信すると同じメッセージを自分宛に返してくれるオウム返しのアプリです。  
+
+### development environment setup
+#### rubyのinstall
+ruby 2.5.8をインストールして下さい。
+
+#### git clone & bundle install
+```
+git clone git@github.com:Nobuo-Hirai/parrot-for-Lambda.git
+```
+
+```
+# gemもLambdaのアプリケーション内に含める必要があるので vendor/bundle 配下へinsallしてください。
+bundle config set --local path 'vendor/bundle'
+bundle install
+```
+
+#### Lambdaの環境変数
+Lambdaの環境変数へセットする内容  
+
+| key | value |
+| ---- | ---- |
+| api_app_id | Slackのapi_app_id (SlackのAppページに表示されているApp ID) |
+| channel_id | 呼び出し元のSlack channel id。対象のchannel以外は実行を許可しない(channel idの確認方法は該当のchannelをアプリで選択して、右クリックでリンクをコピーします。それをブラウザへ貼り付け、/archives/以降がidです。) |
+| notification_slack_channel | Slackの通知先channel名(#hogehoge のような感じ) |
+| slack_token | Slackのaccess token(Bot User OAuth Access Token) |
+
+
+```
+
+## Lambdaへdeploy
+### Lambdaへのdeployはソースをzipにしてアップロードします。
+
+### zip file作成
+```
+zip -r parrot-for-lambda.zip vendor lambda_function.rb
+```
+
---
+++ lambda_function.rb
@@ -0,0 +1,50 @@
+require "bundler/setup"
+require "active_support"
+require "active_support/core_ext"
+require "slack-ruby-client"
+
+def lambda_handler(event:, context:)
+
+  # slackのEvent APIの認証用
+  if event["body"].present? && JSON.parse(event["body"])["challenge"].present?
+    return { statusCode: 200, body: JSON.parse(event["body"])["challenge"] }
+  end
+
+  # slack APIのretryの場合は処理をしない
+  return { statusCode: 200, body: "No need to resend" } if retry_header?(event["headers"])
+
+  json_body = JSON.parse(event["body"])
+  # 対象のslackのbot, channel以外は実行せずalert
+  channel_id = json_body.dig("event", "channel")
+  api_app_id = json_body.dig("api_app_id")
+
+  # 不正なアクセスはcloudwatchで確認
+  if channel_id != ENV["channel_id"] || api_app_id != ENV["api_app_id"]
+    puts "event_data:#{event.inspect}"
+    notification_to_slack("意図しないchannelまたはユーザから実行されました。")
+    return { statusCode: 200, body: "OK" }
+  end
+
+  text = json_body.dig("event", "blocks", 0, "elements", 0, "elements", 1, "text")
+  if text.blank?
+    return { statusCode: 200, body: "OK" }
+  else
+    notification_to_slack(text)
+    return { statusCode: 200, body: "OK" }
+  end
+end
+
+def retry_header?(event_headers)
+  retry_num = event_headers.fetch("X-Slack-Retry-Num", nil)
+  retry_reason = event_headers.fetch("X-Slack-Retry-Reason", nil)
+  retry_num.present? && (retry_reason == "http_timeout" || retry_reason == "http_error")
+end
+
+def notification_to_slack(message)
+  channel = ENV["channel_id"]
+  Slack.configure do |config|
+    config.token = ENV["slack_token"]
+  end
+  client = Slack::Web::Client.new
+  client.chat_postMessage(channel: channel, text: message)
+end
